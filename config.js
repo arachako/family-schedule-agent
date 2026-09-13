2d2f211d86af2650e6305422a57be09cd44d4766
arachako <108241962+arachako@users.noreply.github.com>
2026-09-12 21:57:18 -0700

    Configure Google Calendar OAuth client ID

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Safe to expose in a browser app. This is an OAuth client ID, not a client secret.
-  googleClientId: "PASTE_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com"
+  googleClientId: "455904128632-c8ukc1gqrtelqcpuogi9vte4b1uiqifi.apps.googleusercontent.com"
 };
